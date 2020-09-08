aa92c0d07a1397044b083c3ebde5429a095a9b14
Nils Andresen <nils@nils-andresen.de>
2020-09-08 15:52:29 +0000

    AppVeyor Publish: 299bdfd5fbf250f50bcc847e22645412c4aabe3e added nils-a for code,doc

--- assets/js/searchIndex.js
+++ assets/js/searchIndex.js
@@ -39,14 +39,14 @@ var searchModule = function() {
     a(
         {
             id:0,
-            title:"AsciiDoctorJAliases",
-            content:"AsciiDoctorJAliases",
+            title:"AsciiDoctorJRunnerSettingsExtensions",
+            content:"AsciiDoctorJRunnerSettingsExtensions",
             description:'',
             tags:''
         },
         {
-            url:'/Cake.AsciiDoctorJ/api/Cake.AsciiDoctorJ/AsciiDoctorJAliases',
-            title:"AsciiDoctorJAliases",
+            url:'/Cake.AsciiDoctorJ/api/Cake.AsciiDoctorJ/AsciiDoctorJRunnerSettingsExtensions',
+            title:"AsciiDoctorJRunnerSettingsExtensions",
             description:""
         }
     );
@@ -67,48 +67,6 @@ var searchModule = function() {
     a(
         {
             id:2,
-            title:"AsciiDoctorJRunnerSettingsExtensions",
-            content:"AsciiDoctorJRunnerSettingsExtensions",
-            description:'',
-            tags:''
-        },
-        {
-            url:'/Cake.AsciiDoctorJ/api/Cake.AsciiDoctorJ/AsciiDoctorJRunnerSettingsExtensions',
-            title:"AsciiDoctorJRunnerSettingsExtensions",
-            description:""
-        }
-    );
-    a(
-        {
-            id:3,
-            title:"DocType",
-            content:"DocType",
-            description:'',
-            tags:''
-        },
-        {
-            url:'/Cake.AsciiDoctorJ/api/Cake.AsciiDoctorJ/DocType',
-            title:"DocType",
-            description:""
-        }
-    );
-    a(
-        {
-            id:4,
-            title:"SafeMode",
-            content:"SafeMode",
-            description:'',
-            tags:''
-        },
-        {
-            url:'/Cake.AsciiDoctorJ/api/Cake.AsciiDoctorJ/SafeMode',
-            title:"SafeMode",
-            description:""
-        }
-    );
-    a(
-        {
-            id:5,
             title:"ERuby",
             content:"ERuby",
             description:'',
@@ -122,7 +80,21 @@ var searchModule = function() {
     );
     a(
         {
-            id:6,
+            id:3,
+            title:"SafeMode",
+            content:"SafeMode",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.AsciiDoctorJ/api/Cake.AsciiDoctorJ/SafeMode',
+            title:"SafeMode",
+            description:""
+        }
+    );
+    a(
+        {
+            id:4,
             title:"BuiltinBackend",
             content:"BuiltinBackend",
             description:'',
@@ -131,6 +103,34 @@ var searchModule = function() {
         {
             url:'/Cake.AsciiDoctorJ/api/Cake.AsciiDoctorJ/BuiltinBackend',
             title:"BuiltinBackend",
+            description:""
+        }
+    );
+    a(
+        {
+            id:5,
+            title:"AsciiDoctorJAliases",
+            content:"AsciiDoctorJAliases",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.AsciiDoctorJ/api/Cake.AsciiDoctorJ/AsciiDoctorJAliases',
+            title:"AsciiDoctorJAliases",
+            description:""
+        }
+    );
+    a(
+        {
+            id:6,
+            title:"DocType",
+            content:"DocType",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.AsciiDoctorJ/api/Cake.AsciiDoctorJ/DocType',
+            title:"DocType",
             description:""
         }
     );
