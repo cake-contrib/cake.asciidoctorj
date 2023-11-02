ba4ec2c11560bc2da0c5109deb6aa0ec8977ab6f
GitHub <noreply@github.com>
2023-11-02 21:00:10 +0000

    Continuous Integration Publish: 83386017b5bbaa3ac0419e57099c81b1e1732f25 Merge pull request #232 from cake-contrib/renovate/xunit-dotnet-monorepo
    (maint) Update dependency xunit to v2.6.1

--- assets/js/searchIndex.js
+++ assets/js/searchIndex.js
@@ -39,20 +39,6 @@ var searchModule = function() {
     a(
         {
             id:0,
-            title:"AsciiDoctorJAliases",
-            content:"AsciiDoctorJAliases",
-            description:'',
-            tags:''
-        },
-        {
-            url:'/cake.asciidoctorj/api/Cake.AsciiDoctorJ/AsciiDoctorJAliases',
-            title:"AsciiDoctorJAliases",
-            description:""
-        }
-    );
-    a(
-        {
-            id:1,
             title:"SafeMode",
             content:"SafeMode",
             description:'',
@@ -66,21 +52,7 @@ var searchModule = function() {
     );
     a(
         {
-            id:2,
-            title:"AsciiDoctorJRunnerSettings",
-            content:"AsciiDoctorJRunnerSettings",
-            description:'',
-            tags:''
-        },
-        {
-            url:'/cake.asciidoctorj/api/Cake.AsciiDoctorJ/AsciiDoctorJRunnerSettings',
-            title:"AsciiDoctorJRunnerSettings",
-            description:""
-        }
-    );
-    a(
-        {
-            id:3,
+            id:1,
             title:"ERuby",
             content:"ERuby",
             description:'',
@@ -94,7 +66,7 @@ var searchModule = function() {
     );
     a(
         {
-            id:4,
+            id:2,
             title:"BuiltinBackend",
             content:"BuiltinBackend",
             description:'',
@@ -108,7 +80,7 @@ var searchModule = function() {
     );
     a(
         {
-            id:5,
+            id:3,
             title:"DocType",
             content:"DocType",
             description:'',
@@ -122,7 +94,7 @@ var searchModule = function() {
     );
     a(
         {
-            id:6,
+            id:4,
             title:"AsciiDoctorJRunnerSettingsExtensions",
             content:"AsciiDoctorJRunnerSettingsExtensions",
             description:'',
@@ -131,6 +103,34 @@ var searchModule = function() {
         {
             url:'/cake.asciidoctorj/api/Cake.AsciiDoctorJ/AsciiDoctorJRunnerSettingsExtensions',
             title:"AsciiDoctorJRunnerSettingsExtensions",
+            description:""
+        }
+    );
+    a(
+        {
+            id:5,
+            title:"AsciiDoctorJAliases",
+            content:"AsciiDoctorJAliases",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/cake.asciidoctorj/api/Cake.AsciiDoctorJ/AsciiDoctorJAliases',
+            title:"AsciiDoctorJAliases",
+            description:""
+        }
+    );
+    a(
+        {
+            id:6,
+            title:"AsciiDoctorJRunnerSettings",
+            content:"AsciiDoctorJRunnerSettings",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/cake.asciidoctorj/api/Cake.AsciiDoctorJ/AsciiDoctorJRunnerSettings',
+            title:"AsciiDoctorJRunnerSettings",
             description:""
         }
     );
